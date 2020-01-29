adc6241e733f50fd52d1241da921f37375b721f7
hrexed <henrik.rexed@neotys.com>
2020-01-29 16:27:25 +0100

    patch on issue on the installation process

--- installer/uninstallNeoLoadService.sh
+++ installer/uninstallNeoLoadService.sh
@@ -6,5 +6,5 @@ NL_SERVICE_RELEASE="0.6.0"
 
 echo "Delete neoload-sli-service $NL_SERVICE_RELEASE"
 # to update the link
-kubectl delete -f https://raw.githubusercontent.com/keptn-contrib/neoload-sli-provider/$NL_SERVICE_RELEASE/config/neoloadexecutor/service.yaml --ignore-not-found
-kubectl delete -f https://raw.githubusercontent.com/keptn-contrib/neoload-sli-provider/$NL_SERVICE_RELEASE/config/neoloadexecutor/distributor.yaml --ignore-not-found
+kubectl delete -f https://raw.githubusercontent.com/keptn-contrib/neoload-sli-provider/$NL_SERVICE_RELEASE/config/service.yaml --ignore-not-found
+kubectl delete -f https://raw.githubusercontent.com/keptn-contrib/neoload-sli-provider/$NL_SERVICE_RELEASE/config/distributor.yaml --ignore-not-found
